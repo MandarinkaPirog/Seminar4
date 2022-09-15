8bbbb4e1f87b1f922b68e6a2e9e74f1a3284d14b
Vika <mandarinkavikabakalova@gmail.com>
2022-09-15 18:01:49 +0400

    Задача 29

---
+++ Z29/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size =8;
+int [] array = new int[size];
+array2(array);
+Console.Write( string.Join(",", array));
+void array2 (int[] array)
+{
+    for (int i=0;i < array.Length;i++)
+    array[i]= new Random().Next(0,100);
+}
